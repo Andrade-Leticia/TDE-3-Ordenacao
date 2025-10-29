77d1151a93e718f40575bb90305dcad9bfffc4cd
Leticia <166722628+Andrade-Leticia@users.noreply.github.com>
2025-10-29 11:20:50 -0300

    Update GnomeSort.java
    Added iteration tracking to GnomeSort algorithm.

--- GnomeSort.java
+++ GnomeSort.java
@@ -3,18 +3,26 @@
 
 public class GnomeSort {
     public static int trocasFeitas;
+    public static int iteracoesFeitas;
 
     static void gnomeSort(int tamanho, int[] vetor){
+        trocasFeitas = 0;
+        iteracoesFeitas = 0;
         int i = 0;
-        while (i < tamanho){
-            if (i == 0 || vetor[i - 1] <= vetor[i]){
+        while (i < tamanho) {
+            if (i == 0) {
                 i++;
-            }
-            else {
-                int temp = vetor[i];
-                vetor[i] = vetor[i - 1];
-                vetor[--i] = temp;
-                trocasFeitas++;
+            } else {
+                iteracoesFeitas++; //comparação entre adjacentes
+                if (vetor[i - 1] <= vetor[i]) {
+                    i++;
+                } else {
+                    int temp = vetor[i];
+                    vetor[i] = vetor[i - 1];
+                    vetor[i - 1] = temp;
+                    trocasFeitas++;
+                    i--;
+                }
             }
         }
     }
