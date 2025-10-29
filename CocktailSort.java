db45ad722311e43260cb7d701d29bbda718f1e96
Leticia <166722628+Andrade-Leticia@users.noreply.github.com>
2025-10-29 11:19:44 -0300

    Update CocktailSort.java
    Added iteration tracking to the cocktail sort algorithm.

--- CocktailSort.java
+++ CocktailSort.java
@@ -5,16 +5,21 @@
 
 public class CocktailSort {
     public static int trocasFeitas;
+    public static int iteracoesFeitas;
 
     static void cocktailSort(int tamanho, int[] vetor) {
         trocasFeitas = 0;
+        iteracoesFeitas = 0;
         boolean trocou = true;
         int inicio = 0;
         int fim = tamanho - 1;
 
         while (trocou) {
             trocou = false;
-            for (int i = inicio; i < fim; i++) {
+
+            int i = inicio;
+            while (i < fim) {
+                iteracoesFeitas++; // cada comparação entre pares
                 if (vetor[i] > vetor[i + 1]) {
                     int temp = vetor[i];
                     vetor[i] = vetor[i + 1];
@@ -22,25 +27,27 @@ public class CocktailSort {
                     trocasFeitas++;
                     trocou = true;
                 }
+                i++;
             }
             if (!trocou) {
                 break; //ta no exemplo do slide
             }
-
             trocou = false;
             fim--;
-            
-            for (int i = fim; i > inicio; i--) {
+
+            i = fim;
+            while (i > inicio) {
+                iteracoesFeitas++; // comparação inversa
                 if (vetor[i] < vetor[i - 1]) {
-                        int temp = vetor[i];
-                        vetor[i] = vetor[i - 1];
-                        vetor[i - 1] = temp;
-                        trocasFeitas++;
-                        trocou = true;
+                    int temp = vetor[i];
+                    vetor[i] = vetor[i - 1];
+                    vetor[i - 1] = temp;
+                    trocasFeitas++;
+                    trocou = true;
                 }
+                i--;
             }
             inicio++;
-            
         }
     }
 }
