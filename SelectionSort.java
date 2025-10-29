5fbcd7ec608188fcc68af13b22b0e50df1881dfe
Leticia <166722628+Andrade-Leticia@users.noreply.github.com>
2025-10-29 11:19:14 -0300

    Update SelectionSort.java
    Added iteration count tracking to selection sort.

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,25 +3,33 @@
 
 public class SelectionSort {
     public static int trocasFeitas;
+    public static int iteracoesFeitas;
 
     static void selectionSort(int tamanho, int[] vetor){
         trocasFeitas = 0;
+        iteracoesFeitas = 0;
 
-        for (int i = 0; i < tamanho - 1; i++){
+        int i = 0;
+        while (i < tamanho - 1) {
             int minIndex = i;
-            for (int j = i + 1; j < tamanho; j++){
-                if (vetor[j] < vetor[minIndex]){
+            int j = i + 1;
+
+            while (j < tamanho) {
+                iteracoesFeitas++; //comparação conta como iteração
+                if (vetor[j] < vetor[minIndex]) {
                     minIndex = j;
                 }
+                j++;
             }
 
-            if (minIndex != i){
+            if (minIndex != i) {
                 int temp = vetor[i];
                 vetor[i] = vetor[minIndex];
                 vetor[minIndex] = temp;
                 trocasFeitas++;
             }
 
+            i++;
         }
     }
 }
